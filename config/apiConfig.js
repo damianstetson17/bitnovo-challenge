c1f7763262945bd943a8284df2aa90196362094d
Damian Stetson <damianstetson17@gmail.com>
2023-07-28 17:39:01 -0300

    deleted secrets in repo

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  credential: "7fb3fa18-6761-4bf0-a3a8-1b9e0247a0df",
-  endpoints: { orders_create: "https://payments.smsdata.com/api/v1/orders/" },
-  ws: "wss://payments.smsdata.com/ws/merchant/",
+  credential: "SECRET_TOKEN",
+  endpoints: { orders_create: "CREATE_ORDER_URL" },
+  ws: "WSS_CONNECTION",
 };
